15de3bf1fb03c3c6740da0f4917a1db254976b4c
Bowser <bowser231@yahoo.com>
2019-07-25 20:19:45 -0600

    Commit test

--- HQTest/src/hqtest/HQTest.java
+++ HQTest/src/hqtest/HQTest.java
@@ -1,11 +1,9 @@
 package hqtest;
 
-import ratingTables.RatingTables;
-import coverages.CovSuper;
-import coverages.CoverageRegistration;
+//import ratingTables.RatingTables;
 
-public class HQTest {
-	private final static CovSuper[] reg = CoverageRegistration.getCovs();// Contains all coverage values.
+
+public class HQTest {	
 	public static void main(String[] args) {
 		//Just to get things started. 
         new GUI_Frame().GUI(); 
@@ -13,19 +11,6 @@ public class HQTest {
        	
        
         	//new RatingTables();
-      
-        
-        
-        
-        
-        
-//        for (int i = 0; i < reg.length; i++) {
-//        	if(reg[i].getName()== "Burglar Alarm") {
-//        		new RatingTables(reg[i]);
-//        	}
-//        	
-//        }
-        
         
         
     }
